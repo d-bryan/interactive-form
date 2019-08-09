7e466a87ccdc7085c1f88ab87409b96762004ada
d-bryan <dylan.g.bryan@gmail.com>
2019-08-09 18:41:40 -0400

    mid project backup

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ FSJS project 3 - Interactive Form
 ******************************************/
 // Global Variables
 const colorOption = $('#color');
+const getActivitiesData = $('input[data-day-and-time]');
+const getDataInput = $('input[type="checkbox"]');
+const targetName = $('.activities [name]');
+
 const jsPunsAvailable = [
     $('#color option[value="cornflowerblue"]'),
     $('#color option[value="darkslategrey"]'),
@@ -14,6 +18,14 @@ const heartJSAvailable = [
     $('#color option[value="steelblue"]'),
     $('#color option[value="dimgrey"]')
 ];
+let activitiesCost = 0;
+let activitiesLegend = $('<span id = "activities-subtotal">Your total cost is: $</span>').appendTo('.activities');
+activitiesLegend;
+
+console.log(getDataInput);
+console.log(getActivitiesData);
+console.log(targetName);
+
 
 
 // focus the form on the name field
@@ -63,6 +75,58 @@ $('#design').change(function (e) {
         }
     });
 });
+
+// Create an element to display the activitie cost total
+
+
+// Listen for changes in the activities section
+// Create helpful variables to store important values
+// Update and display the total activities cost
+// Disable conflicting activities
+
+$(getDataInput).on('click', function(e){
+    let checked = $(this).is(':checked');
+    let currentBox = e.target;
+    let currentDataCost = $(currentBox).data('cost');
+    let removePriceSign = currentDataCost.slice(1);
+    let convertINT = parseInt(removePriceSign);
+    
+    if (checked) {
+        activitiesCost += convertINT;
+    } else {
+        activitiesCost -= convertINT
+    }
+    $('#activities-subtotal').text('Your total cost is: $').append(activitiesCost);
+
+    
+    $(getActivitiesData).each(function (index, element) {
+        let getDateTime = $(this).attr('data-day-and-time');
+        let indexTime = $(getDataInput[index]).attr('data-day-and-time');
+        let targetTime = $(e.target).data('dayAndTime');
+        // let targetName = $('.activities name');
+        
+        // console.log($(this).data('dayAndTime'));
+        // console.log('1   ' + getDateTime);
+        console.log('2   ' + indexTime);
+        // console.log(e.target);
+        console.log($(e.target).data('dayAndTime'));
+        
+        if (targetTime === indexTime && e.target.name === targetName[index]) {
+            if (checked) {
+                console.log(this);
+                console.log(e.target.name);
+                $(element).attr('disabled', true);
+            } else {
+                console.log(this);
+                console.log(e.target.name);
+                $(element).attr('disabled', false);
+            }
+        }
+
+    });
+    
+});
+
 
 
 
