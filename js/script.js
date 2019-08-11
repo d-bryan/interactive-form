7a124f27263b69dda062d32f33f831551d9e51da
d-bryan <dylan.g.bryan@gmail.com>
2019-08-11 17:04:25 -0400

    update, finished payment method section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 Treehouse Techdegree:
 FSJS project 3 - Interactive Form
 ******************************************/
-// Global Variables
+/******************************************
+Global Variables
+******************************************/
+
 const colorOption = $('#color');
 const targetName = $('.activities [name]');
 const activitiesInput = $('.activities input');
@@ -18,8 +21,12 @@ const heartJSAvailable = [
     $('#color option[value="dimgrey"]')
 ];
 let activitiesCost = 0;
-let activitiesLegend = $('<span id = "activities-subtotal">Your total cost is: $</span>').appendTo('.activities');
+let activitiesLegend = $('<span id = "activities-subtotal">Your total cost is: $' + activitiesCost + '</span>').appendTo('.activities');
 activitiesLegend;
+
+/******************************************
+Basic Info Section
+******************************************/
 
 // focus the form on the name field
 $('#name').focus();
@@ -37,17 +44,34 @@ $('#title').change(function(){
     }
 });
 
+/******************************************
+T-Shirt Info 
+******************************************/
+
 // hide select theme initially
 $('#design option[value="select-theme"]').attr('disabled',true).attr('hidden', true);
+// hide the colors to only be shown after the user has selected a theme
+$('#color').addClass('is-hidden');
+// Change text to say please select a T-shirt theme
+$('#color').parent().find('label').text('Please select a T-shirt theme:');
 
 // change the colors of the t-shirt based on the theme
 $('#design').change(function (e) {
-    // target theme variable
+    // Refactortored selectors to use throughout function
     let theme = e.target;
+    let jsPUNS = $(theme).val() === 'js puns';
+    let heartJS = $(theme).val() === 'heart js';
+
+    // If the target value is either color then change the display label text and remove the hidden class
+    if (jsPUNS || heartJS) {
+        $('#color').removeClass('is-hidden');
+        $('#color').parent().find('label').text('Color:')
+    }
+
     // Loop over each color option field element
     $(colorOption).each(function () {
         // If the target value equals js puns hide the colors unavailable to this theme
-        if ($(theme).val() === 'js puns') {
+        if (jsPUNS) {
             $(heartJSAvailable).each(function(){
                 $(this).addClass('is-hidden');
             });
@@ -58,18 +82,24 @@ $('#design').change(function (e) {
             });
         }
         // If the target value equals heart js hide the colors unavailable to this theme
-        if ($(theme).val() === 'heart js') {
+        if (heartJS) {
             $(jsPunsAvailable).each(function(){
                 $(this).addClass('is-hidden');
+                // $(this).toggleClass('is-hidden');
             });
         } else {
             // Otherwise remove the class the hides these colors
             $(jsPunsAvailable).each(function(){
                 $(this).removeClass('is-hidden');
+                // $(this).toggleClass('is-hidden');
             });
         }
     });
 });
+
+/******************************************
+Register for Activities
+******************************************/
 
 // click event handler for checkboxes to add up price and disable similar time slots for workshops
 
@@ -112,6 +142,67 @@ $(activitiesField).on('click', function(e){
             }
         }
     });
+});
+
+/******************************************
+Payment Section
+******************************************/
+const creditCard = $('#payment option[value="credit card"]');
+const paypal = $('#payment option[value="paypal"]');
+const bitcoin = $('#payment option[value="bitcoin"]');
+
+// hide the select payment method initially
+$('#payment option[value="select method"]').attr('disabled',true).attr('hidden', true);
+// Set the credit card payment method to selcted intially 
+creditCard.attr('selected', true);
+// Hide the Paypal and Bitcoin divs intially
+$('#paypal').addClass('is-hidden');
+$('#bitcoin').addClass('is-hidden');
+
+$('#payment').change(function(e){
+    let select = e.target;
+
+    // if target value equals credit card
+    if ($(select).val() === creditCard.val()) {
+        // credit card select method true
+        creditCard.attr('selected', true);
+        // other payment methods false
+        paypal.attr('selected', false);
+        bitcoin.attr('selected', false);
+    } else if ($(select).val() === paypal.val()) {
+        // paypal select method true
+        paypal.attr('selected', true);
+        // other payment methods false
+        creditCard.attr('selected', false);
+        bitcoin.attr('selected', false);
+    } else if ($(select).val() === bitcoin.val()) {
+        // bitcoin select method true
+        bitcoin.attr('selected', true);
+        // other payment methods false
+        paypal.attr('selected', false);
+        creditCard.attr('selected', false);
+    }
+
+    // if/else statement to hide payment sections based on which is selected
+    if (creditCard.is(':selected')) {
+        // show payment method
+        $('#credit-card').removeClass('is-hidden');
+        // remove payment method
+        $('#paypal').addClass('is-hidden');
+        $('#bitcoin').addClass('is-hidden');
+    } else if(paypal.is(':selected')) {
+        // show payment method
+        $('#paypal').removeClass('is-hidden');
+        // remove payment method
+        $('#credit-card').addClass('is-hidden');
+        $('#bitcoin').addClass('is-hidden');
+    } else if (bitcoin.is(':selected')) {
+        // show payment method
+        $('#bitcoin').removeClass('is-hidden');
+        // remove payment method
+        $('#paypal').addClass('is-hidden');
+        $('#credit-card').addClass('is-hidden');
+    }
 });
 
 
